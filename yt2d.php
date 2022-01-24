ebc6f210e548ba740d9a58cbd45a496767adec6e
Stvan <istvan.makai@gmail.com>
2022-01-24 18:11:16 +0100

    Update yt2d.php

--- yt2d.php
+++ yt2d.php
@@ -1,7 +1,8 @@
 <?php
 
 $FB_PERSISTENT_KEY ="";
-$search_film_in_youtube   ='seaarch film in youtube';
+
+$search_film_in_youtube   ='search term ';
 $output = shell_exec("python3 y2td_search.py {$search_film_in_youtube}");
 
 $json = json_decode($output,true);
@@ -11,14 +12,18 @@ $arr = $json['result'];
 //get one film link
 
 $link = $arr[array_rand($arr)];
+var_dump($link['title']);
 $link = $link['link'];
+var_dump($link);
+echo shell_exec("python yt2d.py {$link} ");
 
-shell_exec("python yt2d.py {$link}");
 
 
-$fb_stream ='ffmpeg -re -i ./video.mp4 \ ';
-$fb_stream.='-acodec libvo_aacenc  -ar 44100 -b:a 128k -pix_fmt yuv420p -profile:v baseline -s 1280x720 -bufsize 6000k -vb 400k -maxrate 1500k -deinterlace \ ';
-$fb_stream.='-vcodec libx264 -preset veryfast -g 30 -r 30 -f flv '.'"rtmps://live-api-s.facebook.com:443/rtmp/{$FB_PERSISTENT_KEY}"';
+
+
+$fb_stream_feed = "ffmpeg -re -i ./video.mp4 -ar 44100 -b:a 128k -pix_fmt yuv420p -profile:v baseline -s 1280x720 -bufsize 6000k -vb 400k -maxrate 1500k -deinterlace -vcodec libx264 -preset veryfast -g 30 -r 30 -f flv \"rtmps://live-api-s.facebook.com:443/rtmp/{$FB_PERSISTENT_KEY}\"";
+
+
 
 //go to live
-shell_exec($fb_stream);
+shell_exec($fb_stream_feed);
